c282399a698281d39b8ebd8a678881b0abb53705
Hugo Huzan <hhuzan@gmail.com>
2019-11-02 03:39:15 -0300

    -

---
+++ test/fiuba/algo3/tp2/TableroTest.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.tp2;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+
+public class TableroTest {
+
+	@Test
+	public void test() {
+		Tablero tablero = new Tablero();
+		assertNotNull(tablero);	}
+}
