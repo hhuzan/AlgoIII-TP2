37e1f7f246179ddbc84743d8457c1098098a2ad2
Lucas Lemes <lucas.lemesarg@gmail.com>
2019-11-03 18:43:30 -0300

    Test para jugador

--- src/test/java/fiuba/algo3/tp2/JugadorTest.java
+++ src/test/java/fiuba/algo3/tp2/JugadorTest.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.tp2;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
@@ -10,5 +11,24 @@ public class JugadorTest {
 		Jugador jugador = new Jugador();
 		assertNotNull(jugador);
 	}
+
+	@Test
+	public void test01PuedoSacarlePuntosAlJugador() {
+		Jugador jugador = new Jugador();
+		jugador.restarPuntos(10);
+		assertEquals(10, jugador.obtenerPuntos());
+	}
+
+	@Test
+	public void test02PuedoSacarlePuntosDosVecesAlJugador() {
+		Jugador jugador = new Jugador();
+		jugador.restarPuntos(10);
+		jugador.restarPuntos(5);
+		assertEquals(5, jugador.obtenerPuntos());
+	}
+	
+
+
+
 
 }
