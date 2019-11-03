6ee553d21fe326689ef36b6e326d6b5cf7865d39
Hugo Huzan <hhuzan@gmail.com>
2019-11-03 17:45:05 -0300

    agrego Casillero.colocar()

--- src/main/java/fiuba/algo3/tp2/Casillero.java
+++ src/main/java/fiuba/algo3/tp2/Casillero.java
@@ -1,5 +1,13 @@
 package fiuba.algo3.tp2;
 
 public class Casillero {
+	Unidad unidad;
 
+	public void colocar(Unidad unidad) {
+		this.unidad = unidad;
+	}
+
+	public boolean estaVacio() { // TODO refactorizar
+		return unidad == null;
+	}
 }
--- src/test/java/fiuba/algo3/tp2/CasilleroTest.java
+++ src/test/java/fiuba/algo3/tp2/CasilleroTest.java
@@ -1,6 +1,9 @@
 package fiuba.algo3.tp2;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class CasilleroTest {
@@ -9,6 +12,20 @@ public class CasilleroTest {
 	public void test00ConstructorCasilleroNoDevuelveNull() {
 		Casillero casillero = new Casillero();
 		assertNotNull(casillero);
+	}
+
+	@Test
+	public void test01CasilleroNuevoEstaVacio() {
+		Casillero casillero = new Casillero();
+		assertTrue(casillero.estaVacio());
+	}
+
+	@Test
+	public void test02ColocoUnidadCasilleroNoEstaVacio() {
+		Casillero casillero = new Casillero();
+		Unidad soldado = new Soldado();
+		casillero.colocar(soldado);
+		assertFalse(casillero.estaVacio());
 	}
 
 }
