17e1265fc3960d0c9bc0326086ebacf515644494
Hugo Huzan <hhuzan@gmail.com>
2019-11-06 00:07:07 -0300

    -

--- src/test/java/fiuba/algo3/tp2/JugadorTest.java
+++ src/test/java/fiuba/algo3/tp2/JugadorTest.java
@@ -1,9 +1,7 @@
 package fiuba.algo3.tp2;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
