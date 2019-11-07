7e0203e4fcd35c5da8d17e04965cf1452dd9037f
Santiago <santiagogalijps@gmail.com>
2019-11-06 22:44:05 -0300

    Pruebas estados

---
+++ src/test/java/fiuba/algo3/tp2/OcupadoTest.java
@@ -0,0 +1,47 @@
+package fiuba.algo3.tp2;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.Test;
+
+public class OcupadoTest {
+
+	@Test
+	public void test00ConstructorVacioNoDevuelveNull() {
+		Casillero casillero = new CasilleroAliado();
+		Vacio vacio = new Vacio(casillero);
+		assertNotNull(vacio);
+	}
+
+	@Test
+	public void test01EstadoOcupadoNuncaDevuelveVacio() {
+		Casillero casillero = new CasilleroAliado();
+		Ocupado ocupado = new Ocupado(casillero);	
+		assertFalse(ocupado.estaVacio());
+	}
+
+	@Test 
+	public void test02ColocamosUnaEntidadEnUnEstadoOcupadoYArrojaUnaExcepcion() {
+		Casillero casillero = new CasilleroAliado();
+		Ocupado ocupado = new Ocupado(casillero);	
+		Jugador jugador = new Jugador();
+		Entidad entidad = new Aliado(new Soldado(jugador));	
+		assertThrows(CasilleroOcupadoException.class, () -> {
+			ocupado.colocar(entidad);
+		});
+	}
+
+	@Test
+	public void test04MovemosUnaEntidadDesdeUnCasilleroAUnCasilleroOcupadoYArrojaExcepcion() {
+		Casillero casilleroOrigen = new CasilleroAliado();
+		Casillero casilleroDestino = new CasilleroAliado();
+		Ocupado ocupado = new Ocupado(casilleroOrigen);	
+		Entidad entidad = new Aliado(new Soldado(new Jugador()));
+		casilleroOrigen.setEntidad(entidad);
+		assertThrows(CasilleroOcupadoException.class, () -> {
+			ocupado.moverDesde(casilleroOrigen);
+		});
+	}	
+}
