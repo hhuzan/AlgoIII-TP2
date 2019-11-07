3ef36d3894091afaf455ffcb0741c8a0936330de
Lucas Lemes <lucas.lemesarg@gmail.com>
2019-11-07 00:25:19 -0300

    Ordeno directorios 2

---
+++ src/main/java/fiuba/algo3/tp2/Excepciones/TipoNoPuedeAtacarException.java
@@ -0,0 +1,8 @@
+package fiuba.algo3.tp2;
+
+@SuppressWarnings("serial")
+public class TipoNoPuedeAtacarException extends RuntimeException {
+		   public String toString(){
+		     return ("Este tipo de entidad no puede atacar") ;
+		  }
+}
