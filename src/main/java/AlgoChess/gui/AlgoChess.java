4d49e5e66e1cd1a5af844991e24fe4798123fdc1
Santiago <santiagogalijps@gmail.com>
2019-11-18 17:54:10 -0300

    Fxml fix

--- src/main/java/AlgoChess/gui/AlgoChess.java
+++ src/main/java/AlgoChess/gui/AlgoChess.java
@@ -1,7 +1,6 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
