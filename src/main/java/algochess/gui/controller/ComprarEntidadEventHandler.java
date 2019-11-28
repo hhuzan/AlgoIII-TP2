b92887380cfaef7d001eb0b46fb5ffb62ec97566
Hugo Huzan <hhuzan@gmail.com>
2019-11-28 09:36:15 -0300

    -

--- src/main/java/algochess/gui/controller/ComprarEntidadEventHandler.java
+++ src/main/java/algochess/gui/controller/ComprarEntidadEventHandler.java
@@ -2,13 +2,6 @@ package algochess.gui.controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import algochess.engine.entidades.Catapulta;
-import algochess.engine.entidades.Curandero;
-import algochess.engine.entidades.Jinete;
-import algochess.engine.entidades.Soldado;
 import algochess.engine.juego.Juego;
 
 public abstract class ComprarEntidadEventHandler implements EventHandler<ActionEvent> {
@@ -20,25 +13,4 @@ public abstract class ComprarEntidadEventHandler implements EventHandler<ActionE
         this.entidadStr = entidad;
         this.juego = juego;
     }
-
-    // @Override
-    // public void handle(ActionEvent actionEvent) {
-    //     if(entidadStr == "jinete") {
-    //         Jinete jinete = new Jinete();
-    //         juego.comprarEntidad(jinete);
-
-    //     } else if(entidadStr == "soldado") {
-    //         Soldado soldado = new Soldado();
-    //         juego.comprarEntidad(soldado);
-
-    //     } else if(entidadStr == "catapulta") {
-    //         Catapulta catapulta = new Catapulta();
-    //         juego.comprarEntidad(catapulta);
-
-    //     } else if(entidadStr == "curandero") {
-    //         Curandero curandero = new Curandero();
-    //         juego.comprarEntidad(curandero);
-
-    //     }
-    // }
 }
