c148062b2856195d32879f0271dbe82194c61d9e
Hugo Huzan <hhuzan@gmail.com>
2019-11-28 09:44:21 -0300

    -

--- src/main/java/algochess/gui/ExceptionHandler.java
+++ src/main/java/algochess/gui/ExceptionHandler.java
@@ -1,6 +1,12 @@
 package algochess.gui;
 
-import algochess.excepciones.*;
+import algochess.excepciones.CasilleroOcupadoException;
+import algochess.excepciones.CasilleroVacioException;
+import algochess.excepciones.ColocarEntidadException;
+import algochess.excepciones.DineroInsuficienteException;
+import algochess.excepciones.EntidadDeMismaFaccionException;
+import algochess.excepciones.JugadorPerdioException;
+import algochess.excepciones.NoEsTuTurnoException;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
