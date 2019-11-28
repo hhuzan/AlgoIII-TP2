fb0e0d48cbe90fa704f1912691156efeb58d9186
Santiago <santiagogalijps@gmail.com>
2019-11-28 08:27:06 -0300

    Exception handling

--- src/main/java/algochess/engine/juego/Juego.java
+++ src/main/java/algochess/engine/juego/Juego.java
@@ -42,23 +42,13 @@ public class Juego {
 	}
 
 	public void comprarEntidad(int fila, int columna) {
-		Casillero casillero = tablero.getCasilleros()[fila][columna];
-		turno.getJugadorActual().comprarEntidad(vendedor, entidadSeleccionada);
-		turno.colocarEntidad(casillero, entidadSeleccionada);
-	}
-
-//TODO ver si se saca
-	public void comprarEntidad(Jugador jugador, Posicion posicion, Entidad entidad) {
-		// TODO: Ver como obtener casillero dada la posición
-		// Casillero casillero = new Casillero(posicion);
 		try {
-			jugador.comprarEntidad(vendedor, entidad);
-			// turno.colocarEntidad(casillero, jugador, entidad);
+			Casillero casillero = tablero.getCasilleros()[fila][columna];
+			turno.getJugadorActual().comprarEntidad(vendedor, entidadSeleccionada);
+			turno.colocarEntidad(casillero, entidadSeleccionada);
 		} catch (Exception ex) {
-			// Se la enviamos a la GUI para que haga lo que sea
 			throw ex;
 		}
-
 	}
 
 	public Tablero getTablero() {
--- src/main/java/algochess/gui/vista/AlgoChess.java
+++ src/main/java/algochess/gui/vista/AlgoChess.java
@@ -34,95 +34,63 @@ import algochess.gui.vista.VistaCasillero;
 import algochess.engine.juego.Juego;
 import algochess.engine.jugador.Jugador;
 import algochess.engine.tablero.Tablero;
+import algochess.excepciones.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import algochess.gui.ExceptionHandler;
 
-
-public class AlgoChess extends Application  {
+public class AlgoChess extends Application {
     private static final int TAMANIO_CASILLERO = 25;
     private static final int COLUMNAS = 20;
     private static final int FILAS = 20;
+    private ExceptionHandler exceptionHandler;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    /*private BorderPane crearVentanaPrincipal(){
-        BorderPane panel = new BorderPane();
-        panel.setTop(new Label("Top"));
-        panel.setBottom(new Label(  "Bottom"));
-        panel.setLeft(new Label("Left"));
-        panel.setRight(new Label("Right"));
-        panel.setCenter(new Label("Center"));
-        return panel;
-    }*/
-
-    // private GridPane crearVentaDeEntidades(){
-
-    //     GridPane ventas = new GridPane();
-    //     ventas.setPadding(new Insets(10,10,10,10));
-    //     ventas.setVgap(20);
-    //     ventas.setHgap(20);
-
-    //     Rectangle soldadoContainer = new Rectangle(TAMANIO_CASILLERO*2,TAMANIO_CASILLERO*2);
-    //     Rectangle caballoContainer = new Rectangle(TAMANIO_CASILLERO*2,TAMANIO_CASILLERO*2);
-    //     Rectangle curanderoContainer = new Rectangle(TAMANIO_CASILLERO*2,TAMANIO_CASILLERO*2);
-    //     Rectangle catapultaContainer = new Rectangle(TAMANIO_CASILLERO*2,TAMANIO_CASILLERO*2);
-
-    //     Image soldadoNeutroImg = new Image("images/SoldadoNeutro.png");
-    //     Image caballoNeutroImg = new Image("images/CaballoNeutro.png");
-    //     Image curanderoNeutroImg = new Image("images/CuranderoNeutro.png");
-    //     Image catapultaNeutroImg = new Image("images/CatapultaNeutro.png");
-
-    //     soldadoContainer.setFill(new ImagePattern(soldadoNeutroImg));
-    //     caballoContainer.setFill(new ImagePattern(caballoNeutroImg));
-    //     curanderoContainer.setFill(new ImagePattern(curanderoNeutroImg));
-    //     catapultaContainer.setFill(new ImagePattern(catapultaNeutroImg));
-
-    //     GridPane.setConstraints(soldadoContainer,0,0);
-    //     GridPane.setConstraints(caballoContainer,0,1);
-    //     GridPane.setConstraints(curanderoContainer,0,2);
-    //     GridPane.setConstraints(catapultaContainer,0,3);
-
-    //     ventas.getChildren().addAll(soldadoContainer,caballoContainer,curanderoContainer,catapultaContainer);
-
-    //     return ventas;
-    // }
-
     @Override
     public void start(Stage stage) {
+        try {
+            stage.setTitle("AlgoChess - V 1.0");
 
-        stage.setTitle("AlgoChess - V 1.0");
+            VBox welcomeContainer = new VBox(20);
+            welcomeContainer.setAlignment(Pos.CENTER);
 
-        VBox welcomeContainer = new VBox(20);
-        welcomeContainer.setAlignment(Pos.CENTER);
+            Image logo = new Image("images/GAME_LOGO.png");
+            ImageView logoView = new ImageView();
+            logoView.setImage(logo);
+            logoView.setFitWidth(300);
+            logoView.setPreserveRatio(true);
+            logoView.setSmooth(true);
+            logoView.setCache(true);
 
-        Image logo = new Image("images/GAME_LOGO.png");
-        ImageView logoView = new ImageView();
-        logoView.setImage(logo);
-        logoView.setFitWidth(300);
-        logoView.setPreserveRatio(true);
-        logoView.setSmooth(true);
-        logoView.setCache(true);
+            Button botonEntrar = new Button();
+            botonEntrar.setText("Jugar");
+            
+            Button botonInstrucciones = new Button();
+            botonInstrucciones.setText("Instrucciones");
 
-        Button botonEntrar = new Button();
-        botonEntrar.setText("Jugar");
-        
-        Button botonInstrucciones = new Button();
-        botonInstrucciones.setText("Instrucciones");
+            Button botonSalir = new Button();
+            botonSalir.setText("Salir");
 
-        Button botonSalir = new Button();
-        botonSalir.setText("Salir");
+            Scene proximaEscena = crearEscenaEleccionJugadores(stage);
 
-        Scene proximaEscena = crearEscenaEleccionJugadores(stage);
+            BotonProximaEscenaHandler botonEntrarHandler = new BotonProximaEscenaHandler(stage, proximaEscena);
+            botonEntrar.setOnAction(botonEntrarHandler);
 
-        BotonProximaEscenaHandler botonEntrarHandler = new BotonProximaEscenaHandler(stage, proximaEscena);
-        botonEntrar.setOnAction(botonEntrarHandler);
+            welcomeContainer.getChildren().addAll(logoView, botonEntrar, botonInstrucciones, botonSalir);
 
-        welcomeContainer.getChildren().addAll(logoView, botonEntrar, botonInstrucciones, botonSalir);
+            Scene escenaBienvenidos = new Scene(welcomeContainer, 1280, 720);
+            stage.setScene(escenaBienvenidos);
 
-        Scene escenaBienvenidos = new Scene(welcomeContainer, 1280, 720);
-        stage.setScene(escenaBienvenidos);
+            stage.show();
 
-        stage.show();
+        } catch (Exception ex) {
+            // exceptionHandler = new ExceptionHandler();
+            // exceptionHandler.manageException(ex);
+        }
     }
 
     private Scene crearEscenaEleccionJugadores(Stage stage) {
