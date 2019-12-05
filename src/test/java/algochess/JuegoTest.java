8cbc28cc830fd2127a2448fc17301a8b4c2601bd
Lucas Lemes <lucas.lemesarg@gmail.com>
2019-12-05 01:33:28 -0300

    agrego test juego

--- src/test/java/algochess/JuegoTest.java
+++ src/test/java/algochess/JuegoTest.java
@@ -5,7 +5,10 @@ import algochess.engine.entidades.Jinete;
 import algochess.engine.facciones.Faccion;
 import algochess.engine.juego.Juego;
 import algochess.engine.jugador.Jugador;
+import algochess.engine.tablero.Tablero;
+import javafx.scene.control.Tab;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.*;
 
@@ -45,6 +48,56 @@ public class JuegoTest {
 
 
     }
+
+    @Test
+    public void test03LaPrimerFaccionEsAliada(){
+
+
+        Juego juego = new Juego("Lucas", "Guido");
+        assertEquals(Faccion.ALIADOS, juego.getFaccionActual());
+
+
+    }
+
+    @Test
+    public void test04CreoUnJuegoYMeDevuelveUnTablero(){
+
+
+        Juego juego = new Juego("Lucas", "Guido");
+        Tablero tablero = juego.getTablero();
+        assertNotNull(tablero);
+
+    }
+
+    @Test
+    public void test05CreoUnJuegoYPuedoSeleccionarUnaEntidadDeUnCasillero(){
+
+
+        Juego juego = new Juego("Lucas", "Guido");
+       Entidad entidad =  juego.seleccionarEntidad(1,1);
+       assertNotNull(entidad);
+
+    }
+
+    @Test
+    public void test06CreoUnJuegoYNoPuedoCambiarDeTurnoSiElJugadorNoTerminoDeFase(){
+
+        Juego juego = new Juego("Lucas", "Guido");
+        assertFalse(juego.cambiarTurno());
+
+    }
+
+    @Test
+    public void test07CreoUnJuegoYNoPuedoComprarUnaEntidadQueNoExiste(){
+
+        Juego juego = new Juego("Lucas", "Guido");
+        Assertions.assertThrows(NullPointerException.class, () -> {juego.comprarEntidad(1,1);});
+
+
+    }
+
+
+
 
 
 
